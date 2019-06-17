57c876e80a722a6dcc9680f2fea68ba91e57d0b3
xiaocan <1217272889@qq.com>
2019-06-17 22:16:29 +0800

    Add files via upload

---
+++ XCollectionViewLineLayout/XCollectionViewLineLayout.h
@@ -0,0 +1,17 @@
+//
+//  XCollectionViewLineLayout.h
+//  PageTest
+//
+//  Created by 王文科 on 2019/6/17.
+//  Copyright © 2019 xiaocan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCollectionViewLineLayout : UICollectionViewFlowLayout
+
+@end
+
+NS_ASSUME_NONNULL_END
